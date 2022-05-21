476938c123aea747158c8728b9be37be23dcd3e2
Lapertjoseph <lapertjoseph@gmail.com>
2022-05-21 20:14:14 +0200

    server créer

---
+++ app.js
@@ -0,0 +1,7 @@
+const http = require('http');       // appel du module http
+
+const server = http.createServer((req, res) => {        // creation du server avec http.createServer qui prend deux arg req, res
+    console.log(req);
+});
+
+server.listen(3000);  // on définit un listener qui prend un arg:  écoute sur le port 3000
