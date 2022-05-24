c2e535fbc2e2ee95a499ad494096f8ea376256f9
Lapertjoseph <lapertjoseph@gmail.com>
2022-05-24 10:17:26 +0200

    process.exit()

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');                           // appel du module http
 
 const server = http.createServer((req, res) => {        // creation du server avec http.createServer qui prend deux arg req, res
     console.log(req);
+    // process.exit();
 });
 
 server.listen(3000);                                    // on définit un listener qui prend un arg:  écoute sur le port 3000
