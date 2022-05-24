783f8d6563c87ed004d81b533aa408d7ba06d0a7
Lapertjoseph <lapertjoseph@gmail.com>
2022-05-24 10:33:16 +0200

    response under the hood

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http = require('http');                           // appel du module http
 
 const server = http.createServer((req, res) => {        // creation du server avec http.createServer qui prend deux arg req, res
-    console.log(req);
+    console.log(req.url, req.method, req.headers);          
     // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.JS Server !</h1></body>');
+    res.write('</html>');
+
 });
 
-server.listen(3000);                                    // on définit un listener qui prend un arg:  écoute sur le port 3000
+server.listen(3000);                                    // on définit un listener qui prend un arg:  écoute sur le port 3000lo
