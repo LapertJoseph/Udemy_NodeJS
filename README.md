ec75bd3374ca14130254913e81f771b19842ecfd
Lapertjoseph <lapertjoseph@gmail.com>
2022-05-24 11:10:14 +0200

    understandind request response and the file Ssytem

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 <h2> Udemy course nodeJS </h2> 
 <h3> 1) First Section - Basics :</h3>
 <p> a) Create Server and Port Listening</p>
-<p> b) Deep in event loop nodeJS</p>
-
+<p> b) Deep in event loop NODEJS</p>
+<p> b) Request & Response in NODEJS</p>
+<p> b) File System & Input</p>
--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const http = require('http');                           // appel du module http
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {        // creation du server avec http.createServer qui prend deux arg req, res
     const url = req.url;
-    if (url === '/') {
+    const method = req.method;
+    if (url === '/') {                  
         res.write('<html>');
         res.write('<head><title>Enter Message</title><head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
     return res.end();
     }
-
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        // event listener de data (NODEJS)
+        req.on('data', (chunk) => { 
+            console.log(chunk);
+            // Push chunk of code parser in the array                            
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            // create a buffer to store in 'parseBody' and concat and readable. (possible seulement si le chunk est du texte)
+            const parseBody = Buffer.concat(body).toString();
+            // parse the body and split to take only the value
+            const message = parseBody.split('=')[1];
+            //write the content in our file created
+            fs.writeFileSync('message.txt', message);
+        } )                                                     ;
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
